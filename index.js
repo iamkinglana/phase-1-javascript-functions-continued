13972484d20c784f9e257b77898a32867953859e
+ <+>
2023-03-14 23:12:46 +0300

    Functions Continued

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+//Implement a function called saturdayFun:
+function saturdayFun(x){
+    if (typeof x ==='undefined'){
+        x='roller-skate'
+    }
+    
+return 'This Saturday, I want to ' +`${x}` +'!' 
+}
+
+//Implement a function called mondayWork:
+
+function mondayWork(y){
+    if (typeof y ==='undefined'){
+        y='go to the office'
+    }
+return 'This Monday, I will ' +`${y}` +'.' 
+
+}
+
+//Implement a function called wrapAdjective:
+
+
+function wrapAdjective(z){
+
+    return function wrap(txt){
+        return `You are ${z}${txt}${z}!`
+    }
+
+
+}
